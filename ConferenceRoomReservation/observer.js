d51af0bb36c1859fbe34ac81a386e9ac92f16414
ykdman <dbsrudejr300@gmail.com>
2024-02-21 09:14:23 +0900

    add func : 렌더링

--- ConferenceRoomReservation/observer.js
+++ ConferenceRoomReservation/observer.js
@@ -2,14 +2,27 @@ function addConferenceRoom() {
   const conferenceRoomSelectEl = document.getElementById(
     "conference_room_select"
   );
-  const liEl = document.createElement("li");
-  liEl.innerHTML = `Test`;
-  conferenceRoomSelectEl.append(liEl);
+  const selectRoomAside = document.getElementById("select_room");
+  const dummy = ["14-1", "14-2", "14-3"];
+  dummy.forEach((dum) => {
+    const radioEl = `
+		<input type="radio" name="room" value="${dum} id="${dum}"/>
+		<label for="${dum}">${dum}</label>
+		`;
+
+    conferenceRoomSelectEl.innerHTML += radioEl;
+  });
+
+  const confirmBtnEl = document.createElement("button");
+  confirmBtnEl.id = "room_select_btn";
+  confirmBtnEl.innerText = "확인";
+
+  selectRoomAside.appendChild(confirmBtnEl);
 }
 
 let conferenceRoomtitleEl = document.getElementById("conference_select_title");
 let titleObserver = new MutationObserver((mutation) => {
-  if (mutation[0].target.innerHTML === "회의실을 선택") {
+  if (mutation[0].target.innerHTML.includes("회의실을 선택")) {
     addConferenceRoom();
   }
 });
